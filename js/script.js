65dad56b20c17cd75f2fc67a2df7545424be8c80
Jakub Firlej <jakub.firlej@outlook.com>
2019-08-05 20:49:25 +0200

    create function generateTitleLinks and remove li in index.html

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ function titleClickHandler(event){
 
   /* get 'href' attribute from the clicked link */
   let atributSelectior =this.getAttribute("href");
-  console.log(atributSelectior, "->href");
+  //console.log(atributSelectior, "->href");
   /* find the correct article using the selector (value of 'href' attribute) */
 let targetArticle=document.querySelector( atributSelectior);
 console.log(targetArticle);
@@ -34,8 +34,46 @@ console.log(targetArticle);
   targetArticle.classList.add('active');
 }
 
+
+function generateTitleLinks()
+{   /* remove contents of titleList */
+    const removeLinks= document.querySelectorAll('.titles li');
+    for(let removeLink of removeLinks)
+    {
+        removeLink.remove();
+    }
+      /* for each article */
+      let article;
+      const articles=document.querySelectorAll('.post');
+      for(article of articles)
+      {
+         
+       /* get the article id */
+       const articleId = article.getAttribute("id");
+       //console.log(articleId);
+        /* find the title element */
+        const articleTitle=article.querySelector('.post-title').innerHTML;
+      //  console.log(articleTitle);
+         /* create HTML of the link */
+ 
+        const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+      //  console.log(linkHTML);
+ /* insert link into titleList */
+         const insertCode=document.querySelector('.list');
+         insertCode.insertAdjacentHTML('beforeend',linkHTML);
+      }
+     
+        
+
+
+}
+generateTitleLinks();
+
 const links = document.querySelectorAll('.titles a');
+console.log(links);
 
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+
